df7124bfc2d25472dc6f1e2890a6fcd8f97ab597
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-02-19 01:41:10 +0530

    Reused code

--- sorting/mergesort2.java
+++ sorting/mergesort2.java
@@ -21,7 +21,7 @@ public class mergesort2 {
         if (!isSorted(A, mid+1, end)) mergeSort(A, mid + 1, end);
 
         // merge 
-        merge(A, start, mid, end);
+        mergesort.merge(A, start, mid, end);
     }
 
     /*
@@ -39,44 +39,6 @@ public class mergesort2 {
         return isSorted(A, start, mid) && isSorted(A, mid + 1, end) && A[mid].compareTo(A[mid+1]) <= 0;
     }
 
-
-    // A[start, mid] is sorted
-    // A[mid + 1, end] is sorted
-    // merge the two sorted arrays into A using two-pointer approach O(n) time and Left, Right array O(n) space
-    static <T extends Comparable<T>> void merge(T[] A, int start, int mid, int end) {
-
-        // copy A[start, mid] into Left
-        T[] Left = Arrays.copyOfRange(A, start, mid + 1);
-
-        // copy A[mid + 1, end] into Right
-        T[] Right = Arrays.copyOfRange(A, mid + 1, end + 1);
-
-        int i = 0;
-        int j = 0;
-        int k = start;
-        while (i < Left.length && j < Right.length) {
-            if (Left[i].compareTo(Right[j]) <= 0) {
-                A[k] = Left[i];
-                i++;
-            } else {
-                A[k] = Right[j];
-                j++;
-            }
-            k++;
-        }
-
-        while(i < Left.length) {
-            A[k] = Left[i];
-            k++;
-            i++;
-        }
-
-        while (j < Right.length) {
-            A[k] = Right[j];
-            k++;
-            j++;
-        }
-    }
 
     public static void main(String[] args) {
         Integer[] A = {2, 3, 1, 4};
