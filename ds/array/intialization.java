d173221799ad199707d6f6138c72e9eb38a6e33f
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-02-25 14:25:33 +0530

    Array initialization class

---
+++ ds/array/intialization.java
@@ -0,0 +1,18 @@
+package ds.array;
+
+import java.util.Arrays;
+
+public class intialization {
+
+    static void defaultArray() {
+        // int defaults to 0;
+        int[] a = new int[10];
+
+        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void main(String[] args) {
+        defaultArray();
+    }
+}
