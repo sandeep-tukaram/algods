2e36b840bc01fbd55e8b9dfac2aa9f83acc80b28
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-02-26 11:34:15 +0530

    benchmarked shifter java

--- datastructure/array/shifter.java
+++ datastructure/array/shifter.java
@@ -65,9 +65,74 @@ public class shifter {
 
     }
 
+}
+
+// test correctness
+class Correctness {
     public static void main(String[] args) {
         Integer[] A = generator.reverseSortedIntegers(10);
-        iteratively_improved(A, 0, 4, 3);
+        shifter.iteratively_improved(A, 0, 4, 3);
         System.out.println(Arrays.toString(A));
     }
+}
+
+
+// test efficiency
+class Benchmark {
+
+    public static void main(String[] args) {
+        // Test different array sizes
+        int[] sizes = {1000, 10000, 100000};
+        
+        System.out.println("\n\n***** Testing for generated sorted array *****");
+        for (int size : sizes) {
+            Integer[] arr = generator.sortedIntegers(size);
+            test(arr);
+        }
+
+        System.out.println("\n\n***** Testing for randomly generated array *****");
+        for (int size : sizes) {
+            Integer[] arr = generator.randomIntegers(size);
+            test(arr);
+        }
+
+        System.out.println("\n\n***** Testing for generated reverse sorted array *****");
+        for (int size : sizes) {
+            Integer[] arr = generator.reverseSortedIntegers(size);
+            test(arr);
+        }
+    }
+
+    private static void test(Integer[] arr) {
+
+        // Benhcmark1
+        Integer[] arr1 = arr.clone();
+        long startTime = System.nanoTime();
+        shifter.iteratively_improved(arr1, 0, arr.length/10, arr.length/10);
+        long endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1_000_000.0;
+        
+        // Benchmark2
+        Integer[] arr2 = arr.clone();     
+        long startTime2 = System.nanoTime();
+        shifter.incremental(arr2, 0, arr.length/10, arr.length/10);
+        long endTime2 = System.nanoTime();
+        double duration2 = (endTime2 - startTime2) / 1_000_000.0;
+        
+
+        // Benchmark
+        Integer[] arr3 = arr.clone();     
+        long startTime3 = System.nanoTime();
+        shifter.division(arr3, 0, arr.length/10, arr.length/10);
+        long endTime3 = System.nanoTime();
+        double duration3 = (endTime3 - startTime3) / 1_000_000.0;
+
+
+        System.out.printf("Array size: %d\n", arr.length);
+        System.out.printf("Shift iteratively time: %.2f ms\n", duration);
+        System.out.printf("Shift incrementally time: %.2f ms\n", duration2);
+        System.out.printf("Shift by division : %.2f ms\n", duration3);
+        System.out.println("--------------------");
+    }
+
 }
