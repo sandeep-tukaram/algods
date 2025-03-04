fcd58e677b122b48da558caf88d4e03b59951492
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-03-04 17:13:32 +0530

    stack full() shouldn't be part of the API

--- datastructure/stack/api.java
+++ datastructure/stack/api.java
@@ -2,9 +2,10 @@ package datastructure.stack;
 
 public interface api<E> {
 
+
     void push(E e);
     E pop();
     boolean isEmpty();
-    boolean isFull();
+    boolean isFull();    // TODO Full() is implementation limitation should be transparent to the Client actually. 
     E top();
 }
