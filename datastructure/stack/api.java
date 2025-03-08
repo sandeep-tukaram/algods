3572fc1d423fa1291b7c3800a307ba0c3cd80461
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-03-08 19:47:20 +0530

    minor note correction on stack api

--- datastructure/stack/api.java
+++ datastructure/stack/api.java
@@ -6,6 +6,10 @@ public interface api<E> {
     void push(E e);
     E pop();
     boolean isEmpty();
-    // boolean isFull();    // ideally a stack has no concept of full. Array implementation of it does. So full is implementation concept.
     E top();
+
+
+    // NOTES - ideally a stack has no concept of full. Array implementation of it does. So full is implementation concept.
+    // Because its a implementation concept, have commented out the method. Not deleted for reference purposes.
+    // boolean isFull();    
 }
