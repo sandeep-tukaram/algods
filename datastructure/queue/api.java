ecfc0034cf322f6b5c03bf5fbf35054518e2a4e0
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-03-17 22:57:28 +0530

    queue api comments

--- datastructure/queue/api.java
+++ datastructure/queue/api.java
@@ -3,4 +3,11 @@ package datastructure.queue;
 public interface api<E> {    
     void enqueue(E e);
     E dequeue();
+    boolean empty();
+
+    // NOTES - ideally a queue has no concept of full. Array implementation fills up but not LinkedList.
+    // So full() is and implementation concept.  
+    // Not deleted for reference purposes.
+    // boolean isFull();    
+
 }
