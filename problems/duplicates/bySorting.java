bcae6b7f564a1645a11e5c663eb11ce4d3e24b3f
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-03-17 22:22:45 +0530

    mergesort for duplicate identification

--- problems/duplicates/bySorting.java
+++ problems/duplicates/bySorting.java
@@ -20,10 +20,23 @@ public class bySorting {
         return false;
     }
 
+    // T(n) = O(nlgn) + O(n) = O(nlgn) worst case
+    // S(n) = O(n), additional space
+    static <T extends Comparable<T>>  boolean mergesort_check(T[] arr) {
+        // T(n) = O(nlgn), S(n) = O(n)
+        quicksort.sort(arr, 0, arr.length-1);
+
+        // Scan O(n)
+        for (int i =1; i< arr.length; i++) {
+            if (arr[i] == arr[i-1]) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Integer[] arr = generator.randomIntegers(10, 100);
         System.out.println(Arrays.toString(arr));
-        System.out.println(quicksort_check(arr));
+        System.out.println(mergesort_check(arr));
     }
 
 }
